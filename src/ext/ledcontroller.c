c27b5e7962387fe9ab8a39f2d08289807e921d68
Ferdi van der Werf <efcm@vdwerf.eu>
2014-02-13 08:22:40 +0100

    Introduce led controller

---
+++ src/ext/ledcontroller.c
@@ -0,0 +1,12 @@
+/**
+ * @file ledcontroller.c
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.12.0
+ */
+
+#include "ledcontroller.h"
+
---
+++ src/ext/ledcontroller.h
@@ -0,0 +1,17 @@
+/**
+ * @file ledcontroller.h
+ * @brief Handles network calls and sets led brightness
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.12.0
+ */
+
+#ifndef EXT_LEDCONTROLLER_H
+#define EXT_LEDCONTROLLER_H
+
+#include "../config.h"
+
+#endif // EXT_LEDCONTROLLER_H
